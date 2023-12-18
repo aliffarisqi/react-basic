ae0ea8689f923c029f1c34a4bc439db728aeb649
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-18 12:23:16 +0700

    add function to add data to api using post method in axios

--- react-basic/src/container/BlogPost/BlogPost.jsx
+++ react-basic/src/container/BlogPost/BlogPost.jsx
@@ -8,16 +8,47 @@ class BlogPost extends Component {
     post: [],
   };
   getPostApi = () => {
-    axios.get("http://localhost:3004/posts").then((result) => {
-      this.setState({
-        post: result.data,
+    axios
+      .get("http://localhost:3004/posts?_sort=id&_order=desc")
+      .then((result) => {
+        this.setState({
+          post: result.data,
+          formBlogPost: {
+            id: 1,
+            title: "",
+            body: "",
+            userId: 1,
+          },
+        });
       });
-    });
   };
   handleRemove = (data) => {
     axios.delete(`http://localhost:3004/posts/${data}`).then((result) => {
       this.getPostApi();
     });
+  };
+  handleFoormChange = (event) => {
+    let newFormBlogPost = { ...this.state.formBlogPost };
+    let timeStamp = new Date().getTime();
+    newFormBlogPost["id"] = timeStamp;
+    newFormBlogPost[event.target.name] = event.target.value;
+    this.setState({
+      formBlogPost: newFormBlogPost,
+    });
+  };
+  handleSubmit = () => {
+    this.postDataToAPI();
+  };
+  postDataToAPI = () => {
+    axios.post("http://localhost:3004/posts/", this.state.formBlogPost).then(
+      (result) => {
+        console.log(result);
+        this.getPostApi();
+      },
+      (err) => {
+        console.log("err", err);
+      }
+    );
   };
   componentDidMount() {
     // menggunakan fetch
@@ -36,6 +67,32 @@ class BlogPost extends Component {
     return (
       <Fragment>
         <p className="section-title">Blog Post</p>
+        <div className="form-add-post">
+          <label htmlFor="title" className="title">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            className=""
+            name="title"
+            placeholder="masukan title ..."
+            onChange={this.handleFoormChange}
+          />
+          <label htmlFor="body" className="title">
+            Deskripsi
+          </label>
+          <textarea
+            name="body"
+            id="body"
+            cols="30"
+            rows="10"
+            placeholder="add body content"
+            onChange={this.handleFoormChange}></textarea>
+          <button onClick={this.handleSubmit} className="btn btn-save">
+            Simpan
+          </button>
+        </div>
         {this.state.post.map((post) => {
           return <Post key={post.id} data={post} remove={this.handleRemove} />;
         })}
