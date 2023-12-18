e0fd836f787b9ebd12fe19062f24c921d6fdd27d
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-18 16:52:03 +0700

    improve folder structure in react

--- react-basic/src/container/Home/Home.jsx
+++ react-basic/src/container/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from "react";
 import YoutubeComp from "../../component/YoutubeComp/YoutubeComp";
-import Product from "../Product/Product";
-import LifeCycleComp from "../LifeCycleComponent/LifeCycleComp";
-import BlogPost from "../BlogPost/BlogPost";
+import Product from "../pages/Product/Product";
+import LifeCycleComp from "../pages/LifeCycleComponent/LifeCycleComp";
+import BlogPost from "../pages/BlogPost/BlogPost";
 import "./Home.css";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import YoutubePage from "../pages/YoutubeComp/YoutubePage";
 
 class Home extends Component {
   state = {
@@ -20,34 +21,6 @@ class Home extends Component {
   render() {
     return (
       <BrowserRouter>
-        {/* <p>Youtube Component</p>
-        <hr />
-        <YoutubeComp
-          time="23.12"
-          title="belajar dengan prawito"
-          desc="2x ditonton"
-        />
-        <YoutubeComp
-          time="12.32"
-          title="belajar dengan sandhika"
-          desc="4x ditonton"
-        />
-        <YoutubeComp
-          time="08.42"
-          title="belajar dengan ajay"
-          desc="6x ditonton"
-        />
-        <YoutubeComp
-          time="20.52"
-          title="belajar dengan alif"
-          desc="10x ditonton"
-        />
-        <YoutubeComp />
-        <p>Counter</p>
-        <hr />
-
-        
-        <BlogPost /> */}
         <div className="nav">
           <Link to="/" className="link-nav">
             Blog Post
@@ -58,12 +31,16 @@ class Home extends Component {
           <Link to="/lifecycle" className="link-nav">
             Lifecycle
           </Link>
+          <Link to="/youtube" className="link-nav">
+            Youtube
+          </Link>
         </div>
         <div className="home-container">
           <Routes>
             <Route path="/" exact Component={BlogPost}></Route>
             <Route path="/product" Component={Product} />
             <Route path="/lifecycle" Component={LifeCycleComp} />
+            <Route path="/youtube" Component={YoutubePage} />
           </Routes>
         </div>
       </BrowserRouter>
--- react-basic/src/container/pages/BlogPost/BlogPost.jsx
+++ react-basic/src/container/pages/BlogPost/BlogPost.jsx
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import "./BlogPost.css";
-import Post from "../../component/Post/Post";
+import Post from "../../../component/Post/Post";
 import axios from "axios";
 
 class BlogPost extends Component {
