f1e68e9e47497bb455541673f2e0300bb1a68dfc
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-20 15:12:56 +0700

    installing and basic redux implementation in react

--- react-basic/basic-redux-latihan/redux.js
+++ react-basic/basic-redux-latihan/redux.js
@@ -1,24 +1,60 @@
 const redux = require("redux");
-const createStore = redux.createStore;
+const membuatStore = require("redux").legacy_createStore;
+
+const createStore = membuatStore;
 
 const initalState = {
   value: 0,
   age: 17,
 };
 
-//membuat Reducer(agen)
+//2. membuat Reducer(agen)
 //untuk mengupdate store atau state
 const rootReducer = (state = initalState, action) => {
-  return state;
+  console.log(action);
+  // if (action.type === "ADD_AGE") {
+  //   return {
+  //     ...state,
+  //     age: state.age + 1,
+  //   };
+  // }
+  // if (action.type === "CHANGE_VALUE") {
+  //   return {
+  //     ...state,
+  //     value: state.value + action.newvalue,
+  //   };
+  // }
+
+  // menggunakan switch
+  switch (action.type) {
+    case "ADD_AGE":
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+    case "CHANGE_VALUE":
+      return {
+        ...state,
+        value: state.value + action.newvalue,
+      };
+    default:
+      return state;
+  }
 };
 
 //Membuat Store untuk menyimpan Value
-//Store
+//1. Store (wadah besar)
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//dispatching action(task list agen)
-// proses pemanggilan reducer
-
 //subscription
 //proses pemanggilam store yang diinginkan
+store.subscribe(() => {
+  console.log("store change : ", store.getState());
+});
+
+//dispatching action(task list agen)
+// proses pemanggilan reducer
+store.dispatch({ type: "ADD_AGE" });
+store.dispatch({ type: "CHANGE_VALUE", newvalue: 16 });
+console.log(store.getState());
