b5250c84c73fb1e80d4c74c5265dae60b7bb2c71
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-24 06:31:07 +0700

    refactor api foldering using global api in react js

--- react-basic/src/service/index.js
+++ react-basic/src/service/index.js
@@ -1,36 +1,6 @@
-import axios from "axios";
-
-// posts?_sort=id&_order=desc
-const LocalPatch = "http://localhost:3004";
-const OnlineRoot = "https://jsonplaceholder.typicode.com";
-
-const Get = (rootPath, path) => {
-  const promise = new Promise((resolve, reject) => {
-    axios.get(`${rootPath}/${path}`).then(
-      (result) => {
-        resolve(result.data);
-      },
-      (err) => {
-        reject(err);
-      }
-    );
-  });
-  return promise;
-};
-
-const Post = (rootPath, path, data) => {
-  const promise = new Promise((resolve, reject) => {
-    axios.post(`${rootPath}/${path}`, data).then(
-      (result) => {
-        resolve(result);
-      },
-      (err) => {
-        reject(err);
-      }
-    );
-  });
-  return promise;
-};
+import { LocalPatch, OnlineRoot } from "./Config";
+import Get from "./Get";
+import Post from "./Post";
 
 //GET
 const getNewsBlog = () => Get(LocalPatch, "posts?_sort=id&_order=desc");
